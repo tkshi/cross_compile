fa7e06012f98ad95e8b908d1eb05a9edede93ac1
endo <takashi.endo@cluez.jp>
2016-11-06 17:10:24 +0900

    webdriverを追加

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-strin = 'gello'
-f = open('text.txt', 'w') 
-f.write(strin) 
-f.close() 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import platform
+if  platform.system() == 'Windows':
+	CHROMEDRIVER_PATH = "./chromedriver.exe"
+else:
+	CHROMEDRIVER_PATH = "./chromedriver"
+driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
